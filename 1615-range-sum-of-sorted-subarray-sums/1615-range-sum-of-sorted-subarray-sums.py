7da912f1f8cce2747a0b2bb016de7bce6c8c0919
Hyun-hyo-min <hyomin2677@gmail.com>
2024-08-04 20:58:46 +0900

    Time: 345 ms (21.24%), Space: 36.2 MB (81.86%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.py
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.py
@@ -1,13 +1,25 @@
+from heapq import heappush, heappop
+from itertools import accumulate
+
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
-        subarray = []
+        MOD = 10**9 + 7
 
-        for i in range(n):
-            for j in range(i, n):
-                sum_of_subarray = sum(nums[i:j + 1])
-                subarray.append(sum_of_subarray)
+        prefix_sums = list(accumulate(nums, initial=0))
+        min_heap = []
         
-        subarray.sort()
-        result = sum(subarray[left-1:right]) % (10**9+7)
+        for i in range(n):
+            for j in range(i + 1, n + 1):
+                sub_sum = prefix_sums[j] - prefix_sums[i]
+                heappush(min_heap, sub_sum)
+        
+        result = 0
+        for _ in range(left - 1):
+            heappop(min_heap)
+        
+        for _ in range(right - left + 1):
+            result += heappop(min_heap)
+            result %= MOD
         
         return result
+
