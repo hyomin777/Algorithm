3fa4ec718a89af63af3869a580162c291c3424ce
Hyun-hyo-min <hyomin2677@gmail.com>
2024-08-18 02:30:32 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.py
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -1,18 +1,16 @@
 class Solution:
     def integerReplacement(self, n: int) -> int:
-        memo  = {}
+        memo = {}
+        memo[1] = 0
 
-        def memoization(idx):
-            if idx == 1:
-                return 0
-
+        def recursion(idx):
             if idx in memo:
                 return memo[idx]
 
             if idx % 2 == 0:
-                memo[idx] = memoization(idx // 2) + 1 
+                memo[idx] = recursion(idx // 2) + 1 
             else:
-                memo[idx] = min(memoization(idx-1), memoization(idx+1)) + 1
+                memo[idx] = min(recursion(idx-1), recursion(idx+1)) + 1
             
             return memo[idx]
 
