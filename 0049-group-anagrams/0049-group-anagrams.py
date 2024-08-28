006d5c915e68bf1a2b451f2fd003161c4a0b2a6a
Hyun-hyo-min <hyomin2677@gmail.com>
2024-08-28 22:46:12 +0900

    Time: 72 ms (99.33%), Space: 20.6 MB (43.34%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict = {}
+    
+        for word in strs:
+            key = tuple(sorted(word))
+            
+            if key not in dict:
+                dict[key] = []
+            
+            dict[key].append(word)
+
+        return list(dict.values())
