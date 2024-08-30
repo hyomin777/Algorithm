d5047681ad7d12b7f991784fbd839a81365eb6db
Hyun-hyo-min <hyomin2677@gmail.com>
2024-08-30 16:09:57 +0900

    Time: 315 ms (5.16%), Space: 16.6 MB (66.52%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
@@ -4,8 +4,6 @@ class Solution:
 
         for i in range(len(nums)):
             dict[i] = 0
-
-        for i in range(len(nums)):
             for j in range(len(nums)):
                 if i != j:
                     if nums[i] > nums[j]:
