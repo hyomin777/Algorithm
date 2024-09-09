9473af732d62b4bb5efa1d71f3771aa107a719fe
Hyun-hyo-min <hyomin2677@gmail.com>
2024-09-10 08:42:02 +0900

    Time: 38 ms (71.18%), Space: 17.2 MB (30.65%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -13,18 +13,18 @@ class Solution:
         queue = [root]
 
         while queue:
-            level_size = len(queue)
             level = []
 
-            for _ in range(level_size):
+            for _ in range(len(queue)):
                 node = queue.pop(0)
-                level.append(node.val)
 
-                if node.left:
+                if node:
+                    level.append(node.val)
                     queue.append(node.left)
-                if node.right:
                     queue.append(node.right)
-
-            result.append(level)
+            if level:
+                result.append(level)
 
         return result
+
+            
