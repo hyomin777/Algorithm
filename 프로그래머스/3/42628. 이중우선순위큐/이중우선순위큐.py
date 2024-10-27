a7d137a25d5f92b64903693031ee5395da143f9f
Hyun-hyo-min <hyomin2677@gmail.com>
2024-10-27 12:22:06 +0900

    [level 3] Title: 이중우선순위큐, Time: 85.26 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -3,34 +3,40 @@ import heapq
 def solution(operations):
     min_heap = []
     max_heap = []
-    entry_map = set()
+    entry_map = {}
 
     for calc in operations:
         if calc.startswith("I"):
             num = int(calc.split()[1])
             heapq.heappush(min_heap, num)
             heapq.heappush(max_heap, -num)
-            entry_map.add(num)
+            entry_map[num] = entry_map.get(num, 0) + 1
         elif entry_map:
             if calc == "D 1":
-                while max_heap and -max_heap[0] not in entry_map:
+                while max_heap and entry_map.get(-max_heap[0], 0) == 0:
                     heapq.heappop(max_heap)
                 if max_heap:
-                    entry_map.remove(-heapq.heappop(max_heap))
+                    max_val = -heapq.heappop(max_heap)
+                    entry_map[max_val] -= 1
+                    if entry_map[max_val] == 0:
+                        del entry_map[max_val]
             elif calc == "D -1":
-                while min_heap and min_heap[0] not in entry_map:
+                while min_heap and entry_map.get(min_heap[0], 0) == 0:
                     heapq.heappop(min_heap)
                 if min_heap:
-                    entry_map.remove(heapq.heappop(min_heap))
-
-    while min_heap and min_heap[0] not in entry_map:
-        heapq.heappop(min_heap)
-    while max_heap and -max_heap[0] not in entry_map:
-        heapq.heappop(max_heap)
+                    min_val = heapq.heappop(min_heap)
+                    entry_map[min_val] -= 1
+                    if entry_map[min_val] == 0:
+                        del entry_map[min_val]
 
     if not entry_map:
         return [0, 0]
     
+    while min_heap and entry_map.get(min_heap[0], 0) == 0:
+        heapq.heappop(min_heap)
+    while max_heap and entry_map.get(-max_heap[0], 0) == 0:
+        heapq.heappop(max_heap)
+
     max_val = -max_heap[0]
     min_val = min_heap[0]
     
