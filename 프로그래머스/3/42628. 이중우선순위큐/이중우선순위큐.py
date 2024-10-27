250e7408abb10f43cd79058b3ace0a898099734f
Hyun-hyo-min <hyomin2677@gmail.com>
2024-10-27 12:14:52 +0900

    [level 3] Title: 이중우선순위큐, Time: 62.18 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,16 +1,37 @@
+import heapq
+
 def solution(operations):
-    answer = []
-    
+    min_heap = []
+    max_heap = []
+    entry_map = set()
+
     for calc in operations:
         if calc.startswith("I"):
-            answer.append(int(calc[1:]))
-        elif not answer:
-            continue
-        elif calc == "D 1":
-            answer.remove(max(answer))
-        elif calc == "D -1":
-            answer.remove(min(answer))
-            
-    if not answer:
+            num = int(calc.split()[1])
+            heapq.heappush(min_heap, num)
+            heapq.heappush(max_heap, -num)
+            entry_map.add(num)
+        elif entry_map:
+            if calc == "D 1":
+                while max_heap and -max_heap[0] not in entry_map:
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    entry_map.remove(-heapq.heappop(max_heap))
+            elif calc == "D -1":
+                while min_heap and min_heap[0] not in entry_map:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    entry_map.remove(heapq.heappop(min_heap))
+
+    while min_heap and min_heap[0] not in entry_map:
+        heapq.heappop(min_heap)
+    while max_heap and -max_heap[0] not in entry_map:
+        heapq.heappop(max_heap)
+
+    if not entry_map:
         return [0, 0]
-    return [max(answer), min(answer)]
+    
+    max_val = -max_heap[0]
+    min_val = min_heap[0]
+    
+    return [max_val, min_val]
