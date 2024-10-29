f3df6c46f168fb14e5364d717af6068cf2a2621f
Hyun-hyo-min <hyomin2677@gmail.com>
2024-10-29 17:17:42 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,30 +1,21 @@
 from collections import deque
 
 def solution(n, computers):
+    visited = [False] * n
     answer = 0
-    graph = {}
     
-    for i, computer in enumerate(computers):
-        graph[i+1] = []
-        for j, network in enumerate(computer):
-            if network == 1 and i != j:
-                graph[i+1].append(j+1)
-                
-    queue = deque([])
-    visited = [0] * (n+1)
-    
-    for i in range(1, n+1):
-        if visited[i] == 0:
+    for i in range(n):
+        if not visited[i]:
             answer += 1
-            visited[i] = 1
-            queue.append(graph[i])
-        
-        while queue:
-            computers = queue.popleft()
+            queue = deque([i])
+            visited[i] = True
             
-            for computer in computers:
-                if visited[computer] == 0:
-                    visited[computer] = 1
-                    queue.append(graph[computer])
+            while queue:
+                node = queue.popleft()
+
+                for neighbor in range(n):
+                    if computers[node][neighbor] == 1 and not visited[neighbor]:
+                        visited[neighbor] = True
+                        queue.append(neighbor)
     
     return answer
