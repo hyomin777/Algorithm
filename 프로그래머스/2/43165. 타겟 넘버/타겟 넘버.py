9f3fec7ae3e7f67c4f3d6d2cd3ca65c9730aecae
Hyun-hyo-min <hyomin2677@gmail.com>
2024-10-29 15:34:24 +0900

    [level 2] Title: 타겟 넘버, Time: 506.49 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,7 +1,6 @@
 def solution(numbers, target):
     def recursion(num, cnt):
         if cnt == len(numbers):
-            # 목표 숫자를 달성한 경우 1을 반환
             if num == target:
                 return 1
             else:
