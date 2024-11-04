c4f5bbd737bbf6dabd81e2a609b515f8f635b30f
Hyun-hyo-min <hyomin2677@gmail.com>
2024-11-04 17:58:16 +0900

    Time: 231 ms (60.28%), Space: 18.8 MB (93.12%) - LeetHub

---
+++ LeetCode/3163-string-compression-iii/3163-string-compression-iii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def compressedString(self, word: str) -> str:
+        if len(word) == 1:
+            return f"1{word}"
+        
+        result = ''
+        cnt = 1
+
+        for i in range(1, len(word)):
+            prev_char = word[i-1]
+            current_char = word[i]
+            
+            if current_char == prev_char:
+                if cnt >= 9:
+                    result += f"{cnt}{prev_char}"
+                    cnt = 1
+                else:
+                    cnt += 1
+            else:
+                result += f"{cnt}{prev_char}"
+                cnt = 1
+            
+        if word[-1] == word[-2]:
+            result += f"{cnt}{word[-1]}"
+        else:
+            result += f"{cnt}{word[-1]}"
+            
+            
+        return result
