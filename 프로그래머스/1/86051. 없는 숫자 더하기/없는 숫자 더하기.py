c4642b2218806b58e4fd68ed4845f417c935fe0d
Hyun-hyo-min <hyomin2677@gmail.com>
2024-11-07 01:15:38 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,14 +1,6 @@
 def solution(numbers):
-    dict = {}
-    answer = 0
-    
-    for i in range(10):
-        dict[i] = False
-    
-    for num in numbers:
-        dict[num] = True
-    
-    for key, value in dict.items():
-        if not value:
-            answer += key
+    answer=0
+    for i in range(1,10):
+        if i not in numbers:
+            answer += i
     return answer
