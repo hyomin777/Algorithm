0956e840a48385a91891996fb7672df06583aac8
Hyun-hyo-min <hyomin2677@gmail.com>
2024-11-17 16:40:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,26 @@
+def solution(s):
+    answer = ''
+    dict = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    }
+    
+    temp = ''
+    for char in s:
+        if char.isdigit():
+            answer += char
+        else:
+            temp += char
+            if temp in dict: 
+                answer += dict[temp]
+                temp = ''
+                
+    return int(answer)
