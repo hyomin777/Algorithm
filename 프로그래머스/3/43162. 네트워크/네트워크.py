97b0b042fa8da9ace37c5ac01e966b1f5173774b
Hyun-hyo-min <hyomin2677@gmail.com>
2024-11-28 11:44:56 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,21 +1,22 @@
-from collections import deque
+def find_network(computers, is_visited, idx):
+    stack = [idx]
+    while stack:
+        current = stack.pop()
+        
+        for idx, computer in enumerate(computers[current]):
+            if computer and not is_visited[idx]:
+                stack.append(idx)
+                is_visited[idx] = True
+
+    return is_visited
 
 def solution(n, computers):
-    visited = [False] * n
     answer = 0
-    
-    for i in range(n):
-        if not visited[i]:
-            answer += 1
-            queue = deque([i])
-            visited[i] = True
+    is_visited = {i: False for i in range(n)}
             
-            while queue:
-                node = queue.popleft()
+    for i in range(n):
+        if not is_visited[i]:
+            is_visited = find_network(computers, is_visited, i)
+            answer += 1
 
-                for neighbor in range(n):
-                    if computers[node][neighbor] == 1 and not visited[neighbor]:
-                        visited[neighbor] = True
-                        queue.append(neighbor)
-    
     return answer
