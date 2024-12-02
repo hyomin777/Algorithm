ed066f4a4d94debc770f7fbb9ec353f24f756a16
Hyun-hyo-min <hyomin2677@gmail.com>
2024-12-02 22:49:05 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -8,8 +8,8 @@ def solution(a, b, c, d):
     if len(keys) == 1:
         return 1111 * keys[0]
     
-    if len(keys) == 2 and (4 in values or 3 in values):
-        p = keys[values.index(3)] if 3 in values else keys[values.index(4)]
+    if len(keys) == 2 and 3 in values:
+        p = keys[values.index(3)]
         q = keys[values.index(1)]
         return (10 * p + q) ** 2
     
