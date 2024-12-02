c090db1aae9654e6d9d2c1a2e799e9569dd664e9
Hyun-hyo-min <hyomin2677@gmail.com>
2024-12-02 22:30:57 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -0,0 +1,26 @@
+from collections import Counter
+
+def solution(a, b, c, d):
+    counts = Counter([a, b, c, d])
+    keys = list(counts.keys())
+    values = list(counts.values())
+    
+    if len(keys) == 1:
+        return 1111 * keys[0]
+    
+    if len(keys) == 2 and (4 in values or 3 in values):
+        p = keys[values.index(3)] if 3 in values else keys[values.index(4)]
+        q = keys[values.index(1)]
+        return (10 * p + q) ** 2
+    
+    if len(keys) == 2 and values.count(2) == 2:
+        p, q = keys
+        return (p + q) * abs(p - q)
+    
+    if len(keys) == 3:
+        p = keys[values.index(2)]
+        q, r = [key for key in keys if key != p]
+        return q * r
+    
+    if len(keys) == 4:
+        return min(keys)
