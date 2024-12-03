0e17e46604f645ac3399215761abde737a9487f4
Hyun-hyo-min <hyomin2677@gmail.com>
2024-12-04 06:04:04 +0900

    [level 1] Title: 과일 장수, Time: 133.67 ms, Memory: 21.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,14 @@
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse=True)
+    
+    for i in range(0, len(score), m):
+        apples = score[i:i + m]
+        
+        if len(apples) < m:
+            continue
+        
+        min_apple = min(apples)
+        answer += min_apple * m
+        
+    return answer
