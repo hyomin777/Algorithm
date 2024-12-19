0f743ae36f7b4162e126fff71f4faea0cc496bfa
Hyun-hyo-min <hyomin2677@gmail.com>
2024-12-19 20:31:32 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    arr = ['a', 'e', 'i', 'o', 'u']
+    answer = ''
+    
+    for char in my_string:
+        if not char in arr:
+            answer += char
+    
+    return answer
