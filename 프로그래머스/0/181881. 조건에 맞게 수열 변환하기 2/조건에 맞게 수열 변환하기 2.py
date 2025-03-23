957035586b3ee240ec54efce3b1f925da3c7201d
hyomin777 <hyomin2677@gmail.com>
2025-03-23 16:27:30 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 199.16 ms, Memory: 23.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -0,0 +1,21 @@
+def solution(arr):
+    answer = []
+    cnt = -1
+
+    for num in arr:
+        if num >= 50 and num % 2 == 0:
+            answer.append(num//2)
+        elif num < 50 and num % 2 != 0:
+            answer.append(num*2+1)
+                
+    while answer != arr:
+        arr = answer
+        answer = []
+        cnt += 1
+        for num in arr:
+            if num >= 50 and num % 2 == 0:
+                answer.append(num//2)
+            elif num < 50 and num % 2 != 0:
+                answer.append(num*2+1)
+        
+    return cnt
