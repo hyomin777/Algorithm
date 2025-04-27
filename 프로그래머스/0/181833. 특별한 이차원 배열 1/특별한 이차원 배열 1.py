93245e9e9fe2608e8b522a3449fb5034ec6a3175
hyomin777 <hyomin2677@gmail.com>
2025-04-28 07:55:00 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.06 ms, Memory: 9.86 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -2,8 +2,6 @@ def solution(n):
     answer = [[0] * n for _ in range(n)]
     
     for i in range(n):
-        for j in range(n):
-            if i == j:
-                answer[i][j] = 1
+        answer[i][i] = 1
             
     return answer
