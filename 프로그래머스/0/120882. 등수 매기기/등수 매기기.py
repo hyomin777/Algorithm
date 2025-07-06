938a2633206e750da1b5a737c1e091676e3b1312
hyomin777 <hyomin2677@gmail.com>
2025-07-06 17:34:35 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -0,0 +1,9 @@
+def solution(score):
+    rank = sorted([sum(s) for s in score], reverse = True)
+    
+    rankDict = {}
+    for i, r in enumerate(rank):
+        if r not in rankDict.keys():
+            rankDict[r] = i + 1
+            
+    return [rankDict[sum(s)] for s in score]
