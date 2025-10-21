99503f298e21ad8812b65cb79c6e7cf696c94d94
hyomin777 <hyomin2677@gmail.com>
2025-10-21 21:32:29 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.cpp
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.cpp
@@ -8,14 +8,10 @@ int solution(string my_string) {
     int answer = 0;
     int num = 0;
     char op = 0;
-    // 1. stringstream 객체에 문자열을 넣습니다.
-    stringstream ss(my_string);
     
-    // 2. 첫 번째 숫자를 읽어서 answer에 저장합니다.
+    stringstream ss(my_string);
     ss >> answer;
     
-    // 3. (연산자)와 (숫자)가 순서대로 계속 읽어지는 동안 반복합니다.
-    // 예: "+ 5", "- 10" ...
     while (ss >> op >> num) {
         if (op == '+') {
             answer += num;
