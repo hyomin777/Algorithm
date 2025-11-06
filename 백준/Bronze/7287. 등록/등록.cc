ade3a4c7ac78fc9c46b48308a8edbd4e26915c6f
hyomin777 <hyomin2677@gmail.com>
2025-11-06 10:46:51 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+        int solved = 159;
+        char id[10] = "kimin0404";
+        printf("%d\n",solved);
+        printf("%s\n", id);
+
+        return 0;
+}
