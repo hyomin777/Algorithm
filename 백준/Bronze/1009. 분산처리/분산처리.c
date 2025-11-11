7f27f0e92116d841220a161336c67f34c55b3af0
hyomin777 <hyomin2677@gmail.com>
2025-11-11 20:15:27 +0900

    [Bronze II] Title: 분산처리, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.c
+++ 백준/Bronze/1009. 분산처리/분산처리.c
@@ -5,12 +5,15 @@ int main() {
         int t;
         scanf("%d", &t);
 
-        int a, b;
-        int total_data;
-
         for(int i = 0; i < t; i++) {
+                int a, b;
                 scanf("%d %d", &a, &b);
+
                 int computer = 1;
+
+                a = a % 10;
+                b = b % 4;
+                if(b == 0) b = 4;
 
                 for(int j = 0; j < b; j++) {
                         computer = (computer * a) % 10;
