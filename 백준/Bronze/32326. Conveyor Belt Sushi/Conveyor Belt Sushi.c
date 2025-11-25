3db9078dd479d492e087063367145217f892a94b
hyomin777 <hyomin2677@gmail.com>
2025-11-25 11:58:06 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+        int R, G, B;
+        scanf("%d", &R);
+        scanf("%d", &G);
+        scanf("%d", &B);
+
+        printf("%d\n", R*3+G*4+B*5);
+        return 0;
+}
